bcf2e0ff0311859e304ac6f64f767103a524b031
yj-603-16 <yj-603-16@DESKTOP-PFK09ET>
2023-07-20 09:29:46 +0900

    gitproject04

--- src/gitproject/hellooo.java
+++ src/gitproject/hellooo.java
@@ -1,7 +1,6 @@
 package gitproject;
 
-public class Helloo {
-
+public class hellooo {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!!!");
 		System.out.println("헬로월드!!");
