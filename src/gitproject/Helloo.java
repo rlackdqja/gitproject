c38dd30b1e9443b7c27f17d48eced0f54c2c5008
yj-603-16 <yj-603-16@DESKTOP-PFK09ET>
2023-07-20 09:27:29 +0900

    gitproject2

--- src/gitproject/Helloo.java
+++ src/gitproject/Helloo.java
@@ -3,7 +3,8 @@ package gitproject;
 public class Helloo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("HelloWorld!!!");
+		System.out.println("헬로월드!!!");
 
 	}
 
